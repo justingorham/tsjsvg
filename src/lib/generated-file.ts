a5a3d2eb78b21e053f996a2f2f6257dd2e9ef04d
Justin Gorham <justingorham@gmail.com>
2020-03-04 18:55:55 -0500

    Validator hooks (#1)
    * package and README updates
    
    * generated-file updated

--- src/lib/generated-file.ts
+++ src/lib/generated-file.ts
@@ -10,39 +10,59 @@ type validatorFunction = (
   | Promise<{ valid: boolean; errors?: any }>;
 
 let defaultValidator: validatorFunction = () => {
-  throw new Error('Default validator not set')
-}
+  throw new Error("Default validator not set");
+};
 
 export const defaults = {
   get validator() {
-    return defaultValidator
+    return defaultValidator;
   },
   set validator(val) {
-    defaultValidator = val
-  },
-}
+    defaultValidator = val;
+  }
+};
 
-export const schema = {definitions: {foo: 'bar'}}
+export const schema = { definitions: { foo: "bar" } };
 
 export type DefinitionKeys = keyof typeof schema.definitions;
 
 export class ModelValidator {
   public static for(def: DefinitionKeys, validatorFunc?: validatorFunction) {
-    const validator = new ModelValidator(def, validatorFunc)
-    return {validate: (data: any) => validator.isValidJson(data)}
+    const validator = new ModelValidator(def, validatorFunc);
+    return { validate: (data: any) => validator.isValidJson(data) };
   }
+
+  private otherValidators: ((data: any) => void | Promise<void>)[] = [];
 
   constructor(
     private def: DefinitionKeys,
-    private validator?: validatorFunction,
+    private validator?: validatorFunction
   ) {}
 
-  public isValidJson(data: any) {
-    const validator = this.validator || defaultValidator
-    const validationSchema = {
-      ...schema,
-      $ref: `#/definitions/${this.def}`,
+  public async isValidJson(data: any) {
+    try {
+      const validator = this.validator || defaultValidator;
+      const validationSchema = {
+        ...schema,
+        $ref: `#/definitions/${this.def}`
+      };
+      const isValid = await Promise.resolve(validator(data, validationSchema));
+      await Promise.all(
+        this.otherValidators.map(v => Promise.resolve(v(data)))
+      );
+      return isValid;
+    } catch (error) {
+      return {
+        valid: false,
+        error
+      };
     }
-    return Promise.resolve(validator(data, validationSchema))
+  }
+
+  public addValidator(v: (data: any) => void | Promise<void>) {
+    this.otherValidators = [...this.otherValidators, v];
+    return () => {
+      this.otherValidators = this.otherValidators.filter(o => o !== v);
+    };
   }
 }
